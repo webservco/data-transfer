428774738a6a7a2e1b26924a2abb966b3a31fbe1
GitHub Action <action@github.com>
2023-07-20 12:49:37 +0000

    release PHP 7.4 downgraded v0.1.0

--- src/WebServCo/DataTransfer/DataTransferObject/KeyValue/StringString.php
+++ src/WebServCo/DataTransfer/DataTransferObject/KeyValue/StringString.php
@@ -8,7 +8,17 @@ use WebServCo\DataTransfer\Contract\DataTransferInterface;
 
 final class StringString implements DataTransferInterface
 {
-    public function __construct(public readonly string $key, public readonly string $value)
+    /**
+     * @readonly
+     */
+    public string $key;
+    /**
+     * @readonly
+     */
+    public string $value;
+    public function __construct(string $key, string $value)
     {
+        $this->key = $key;
+        $this->value = $value;
     }
 }
